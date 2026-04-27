830017bf3f45ca4a8e9b08a0687e4b90390b6919
Max-Negmatzoda <m97152909@gmail.com>
2026-04-27 22:44:10 +0500

    Delete TSIS/Phonebook/config.py

--- TSIS/Phonebook/config.py
+++
@@ -1,8 +0,0 @@
-def load_config():
-    return {
-        "dbname": "postgres",
-        "user": "postgres",
-        "password": "270091",
-        "host": "localhost",
-        "port": "5432"
-    }
